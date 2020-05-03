2654a2e2d3d1027f2fa5a22973be137ccd6af0f3
Rebecca <beccadsouza16@gmail.com>
2020-05-03 19:39:51 +0530

    4/7/20

---
+++ src/_april7/CountingElements.java
@@ -0,0 +1,26 @@
+package _april7;
+
+import java.util.*;
+
+/*
+ * Created by Rebecca Dsouza on 4/7/20
+ */
+public class CountingElements {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.countElements(new int[]{1,2,3}));
+        System.out.println(sol.countElements(new int[]{1,1,3,3,5,5,7,7}));
+        System.out.println(sol.countElements(new int[]{1,3,2,3,5,0}));
+        System.out.println(sol.countElements(new int[]{1,1,2,2}));
+    }
+}
+
+class Solution {
+    public int countElements(int[] arr) {
+        HashSet<Integer> hs = new HashSet<>();
+        for(int x: arr) hs.add(x);
+        int ans = 0;
+        for(int x:arr) if(hs.contains(x+1)) ans ++;
+        return ans;
+    }
+}

c9e9022ac2dc3bb3e1da7c5a8b73100bf5a7691c
Rebecca <beccadsouza16@gmail.com>
2020-09-07 13:21:45 +0530

    9/6/20

---
+++ src/_835/ImageOverlap.java
@@ -0,0 +1,42 @@
+package _835;
+/*
+* Created by Rebecca Dsouza on 9/6/20
+*/
+
+public class ImageOverlap {
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        System.out.println(sol.largestOverlap(new int[][]{{1,1,0},{0,1,0},{0,1,0}},new int[][]{{0,0,0},{0,1,1},{0,0,1}}));
+    }
+}
+class Solution {
+    protected int shiftAndCount(int xShift, int yShift, int[][] M, int[][] R) {
+        int count = 0;
+        int rRow = 0;
+        // count the cells of ones in the overlapping zone.
+        for (int mRow = yShift; mRow < M.length; ++mRow) {
+            int rCol = 0;
+            for (int mCol = xShift; mCol < M.length; ++mCol) {
+                if (M[mRow][mCol] == 1 && M[mRow][mCol] == R[rRow][rCol])
+                    count += 1;
+                rCol += 1;
+            }
+            rRow += 1;
+        }
+        return count;
+    }
+
+    public int largestOverlap(int[][] A, int[][] B) {
+        int maxOverlaps = 0;
+
+        for (int yShift = 0; yShift < A.length; ++yShift)
+            for (int xShift = 0; xShift < A.length; ++xShift) {
+                // move one of the matrice up and left and vice versa.
+                // (equivalent to move the other matrix down and right)
+                maxOverlaps = Math.max(maxOverlaps, shiftAndCount(xShift, yShift, A, B));
+                maxOverlaps = Math.max(maxOverlaps, shiftAndCount(xShift, yShift, B, A));
+            }
+
+        return maxOverlaps;
+    }
+}
